2b58e54476f266e7d37b62e0766014748d3e83a2
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 20:22:52 +0600

    mod3task6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        long check = (long) numberToBeRounded;
+        float comp = (float)check;
+        comp += 0.5f;
+
+        if(numberToBeRounded >= comp) check++;
+        System.out.println(check);
     }
+//
+//    public static void main(String[] args) {
+//        new FloatTypeCasting().roundNumber(3.3f);
+//        new FloatTypeCasting().roundNumber(3.8f);
+//    }
 }
